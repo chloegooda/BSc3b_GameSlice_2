f3866905e74a0b86594ad3d60a6bab1d5f7ccc17
chloegooda <43572748+chloegooda@users.noreply.github.com>
2021-04-12 17:52:47 +0100

    - Coin item C++ updates - basic setup

--- Source/GameSlice/CoinItem.cpp
+++ Source/GameSlice/CoinItem.cpp
@@ -2,26 +2,15 @@
 
 
 #include "CoinItem.h"
+#include "ConstructorHelpers.h"
+#include "UObject/UObjectGlobals.h"
+#include "Components/BoxComponent.h"
 
 // Sets default values
 ACoinItem::ACoinItem()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
-}
-
-// Called when the game starts or when spawned
-void ACoinItem::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void ACoinItem::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
+    // generate mesh component and set as root
+    this->MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(FName("MeshComponent"));
+    this->RootComponent = this->MeshComponent;
 }
 
--- Source/GameSlice/CoinItem.h
+++ Source/GameSlice/CoinItem.h
@@ -4,7 +4,38 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Engine/DataTable.h"
 #include "CoinItem.generated.h"
+
+UENUM(BlueprintType)
+enum class CoinType : uint8 {
+	SingleCoin,
+	SmallHeap,
+	LargeHeap,
+	SmallStack,
+	LargeStack,
+	AlmostEmptyBag,
+	Bag,
+	FullBag
+};
+
+USTRUCT(BlueprintType)
+struct FCoinInfo : public FTableRowBase
+{
+	GENERATED_BODY();
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		int ID;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FString name;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		CoinType cointype = CoinType::SingleCoin;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		int value;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UStaticMesh* mesh;
+};
 
 UCLASS()
 class GAMESLICE_API ACoinItem : public AActor
@@ -12,15 +43,18 @@ class GAMESLICE_API ACoinItem : public AActor
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
+
 	ACoinItem();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FCoinInfo CoinInfo;
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+		UStaticMeshComponent* MeshComponent;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+		void PickUpItem();
+	virtual void PickUpItem_Implementation() {
+		this->Destroy();
+	}
 
 };
